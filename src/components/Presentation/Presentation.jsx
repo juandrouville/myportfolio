d3d4011f69230b7ce331db43651cafd9f1d2c469
juandrouville <jadrouville@gmail.com>
2023-03-14 18:00:31 -0300

    change photo and add text home

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -8,9 +8,11 @@ function Presentation(){
             <div className="conteinerText">
                 <h1 className="profession">DESIGNER</h1>
                 <h1 className="profession">DEVELOPER</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias corrupti 
-                    ducimus provident quidem eius est fugiat adipisci, aliquam quas nihil, omnis consequuntur 
-                    commodi labore laboriosam doloribus odio asperiores a culpa.
+                <p>
+                    Bienvenidos a mi portfolio, soy Juan Drouville.
+                    Aquí muestro cada experiencia y herramienta que fui adquiriendo y que puedo ofrecer.
+                    Siempre sentí la necesidad de no quedarme cómodo y de buscar mi lugar. 
+                    Ahora encontré mi lugar, como programador y diseñador, y es donde me quiero desarrollar y seguir aprendiendo,crecer profesionalmente y completar este portfolio con grandes proyectos y buenos resultados.    
                 </p>
             </div>
             <div className="conteinerImg">
@@ -21,3 +23,4 @@ function Presentation(){
 };
 
 export default Presentation;
+  
