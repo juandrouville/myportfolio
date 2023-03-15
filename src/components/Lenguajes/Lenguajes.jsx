382fa1a2606dd11c329474a2cd434033f6394a58
juandrouville <jadrouville@gmail.com>
2023-03-15 13:30:30 -0300

    setLanguage and responsive line

--- src/components/Lenguajes/Lenguajes.jsx
+++ src/components/Lenguajes/Lenguajes.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Lenguajes.css";
+import { Language } from '../../context/languageContext';
 
 function Lenguajes(){
+    const { setLanguage } = useContext(Language);
+    const changeLanguage= (event) => {
+        var language = event.target.id;
+        setLanguage(language);
+    }
     return(
         <div className="conteinerLenguajes">
-            <h1>ESP  |  </h1>
-            <h1>POR  |  </h1>
-            <h1>ING </h1>
+            <h1 id='esp' onClick={changeLanguage}>ESP </h1>
+            <h1 style={{ marginRight:'5px',marginLeft:'5px'}}>|</h1>
+            <h1 id='por' onClick={changeLanguage}>POR </h1>
+            <h1 style={{ marginRight:'5px',marginLeft:'5px'}}>|</h1>
+            <h1 id='ing' onClick={changeLanguage}>ING </h1>
         </div>
     );
 };
