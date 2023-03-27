da23a046d3739ae7a2fbba3e3688598fa3ec0938
juandrouville <jadrouville@gmail.com>
2023-03-27 17:52:47 -0300

    change style languaje active

--- src/components/Lenguajes/Lenguajes.jsx
+++ src/components/Lenguajes/Lenguajes.jsx
@@ -3,18 +3,34 @@ import "./Lenguajes.css";
 import { Language } from '../../context/languageContext';
 
 function Lenguajes(){
-    const { setLanguage } = useContext(Language);
+    const { setLanguage , language } = useContext(Language);
+    console.log(language);
     const changeLanguage= (event) => {
         var language = event.target.id;
         setLanguage(language);
     }
     return(
         <div className="conteinerLenguajes">
-            <h1 id='esp' onClick={changeLanguage}>ESP </h1>
+            { language === 'esp' ? 
+                ( 
+                    <h1 id='esp' onClick={changeLanguage} style={{color:"#ff5613"}} >ESP </h1>
+                ):(
+                <h1 id='esp' onClick={changeLanguage}>ESP </h1>
+                )
+            }
             <h1 style={{ marginRight:'15px',marginLeft:'15px'}}>|</h1>
-            <h1 id='por' onClick={changeLanguage}>POR </h1>
+            {
+                language === 'por' ? 
+                (
+                    <h1 id='por' onClick={changeLanguage} style={{color:"#ff5613"}} >POR </h1>
+                ) : (
+                    <h1 id='por' onClick={changeLanguage}> POR </h1> 
+                )
+            }
+            
         </div>
     );
 };
 
 export default Lenguajes;
+           
