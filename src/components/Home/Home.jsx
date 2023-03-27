c95620bc18b7425ba90cddc8ef542c8399e857b0
juandrouville <jadrouville@gmail.com>
2023-03-27 13:58:54 -0300

    modal download

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,6 @@ import './Home.css';
 import Nav from "../Nav/Nav";
 import SubNav from "../SubNav/SubNav";
 import Presentation from "../Presentation/Presentation";
-import pdf from '../../Curriculo-Juan-Drouville-por.pdf'
 
 function Home(){
     return (
@@ -11,9 +10,6 @@ function Home(){
             <Nav/>
             <SubNav/>
             <Presentation/>
-            <a href={pdf} target='_blank'rel="noopener noreferrer" download="Curriculo-Juan-Drouville-por.pdf">
-            Dowload    
-            </a>
         </div>
     );
 };
--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -3,7 +3,7 @@ import "./Presentation.css";
 import img from '../../portfolio.jpg';
 import { Language } from '../../context/languageContext';
 import { Link } from 'react-router-dom';
-
+import Modal from "../Modal/Modal";
 
 function Presentation(){
     const { language }= useContext(Language);
@@ -11,6 +11,11 @@ function Presentation(){
         esp:'Bienvenidos a mi portfolio, soy Juan Drouville. Aquí muestro cada experiencia y herramienta que fui adquiriendo y que puedo ofrecer. Siempre sentí la necesidad de no quedarme cómodo y de buscar mi lugar.  Ahora encontré mi lugar, como programador y diseñador, y es donde me quiero desarrollar y seguir aprendiendo,crecer profesionalmente y completar este portfolio con grandes proyectos y buenos resultados.',
         por:'Bem-vindo ao meu portfólio, sou Juan Drouville. Aqui mostro cada experiência e ferramenta que fui adquirindo e que posso oferecer. Sempre senti a necessidade de não ficar confortável e de encontrar o meu lugar. Agora encontrei meu lugar, como programador e designer, e é onde quero me desenvolver e continuar aprendendo, crescer profissionalmente e completar este portfólio com grandes projetos e bons resultados.',
     };
+    const download = {
+        esp:'DOWNLOAD CURRICULUM',
+        por:'DOWNLOAD CURRÍCULO',
+    };
+    const [openModal , setOpenModal]= React.useState(false);
     return(
         <div className="conteinerPresentation">
             <div className="conteinerText">
@@ -19,6 +24,10 @@ function Presentation(){
                     <h1 className="professionTitle">DEVELOPER</h1>
                 </Link>
                 <p className="textPresentation">{ myText[language] }</p>
+                <button className="dowloadButton" onClick={()=>setOpenModal(true)}>{download[language]}</button>
+                <Modal open = {openModal} onClickClose={()=>setOpenModal(false)}>
+
+                </Modal>
             </div>
             <div className="conteinerImg">
                 <img src={img} alt='My photo portfolio'/>
