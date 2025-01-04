333c34b212a21d91594eb973363d610365ff9cd0
juandrouville <jadrouville@gmail.com>
2025-01-04 20:56:33 -0300

    animate dismunt modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import './Modal.css';
 import { gsap } from 'gsap';
@@ -9,15 +9,23 @@ const Modal = (props) => {
     const { open, onClickClose, children } = props;
 
     useEffect(()=>{
-        gsap.to(".conteinerOverley",{opacity:1,ease:'power1.inOut'});
+        gsap.to(".conteinerOverlay",{opacity:1,ease:'power1.inOut'});
         gsap.to(".conteinerModal",{ease:'power1.inOut',delay:0.2,y:'0vh'});
-    })
+    });
+
+    async function dismount(){
+        console.log("animateDismount");
+        await gsap.to(".conteinerModal",{ease:'power1.inOut',y:'-100vh'});
+        await gsap.to(".conteinerOverlay",{opacity:0,ease:'power1.inOut',duration:0.5});
+        onClickClose();
+    };
+
     if(!open){ return null };
         return ReactDOM.createPortal(
-            <div className="conteinerOverley">
+            <div className="conteinerOverlay" onClick={()=> dismount()}>
                 <div className="conteinerModal">
                     <div className="borderModal">
-                        <div className="toClose" onClick={()=>onClickClose()}><h1>X</h1></div>
+                        <div className="toClose" onClick={()=> dismount() }><h1>X</h1></div>
                         <div className="conteinerChildren">
                             {children}
                         </div>
