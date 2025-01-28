10067c0f65f91a2e266e424a4ba242f834569583
juandrouville <jadrouville@gmail.com>
2025-01-28 19:14:09 -0300

    changes responsive

--- src/components/Gradient/Gradient.jsx
+++ src/components/Gradient/Gradient.jsx
@@ -4,11 +4,11 @@ import { gsap } from "gsap";
 
 function Gradient(){
     useEffect(()=>{
-        let tl = gsap.timeline();
-        tl.to(".gradient",{ x:0,opacity:0.8,duration:2,ease:'power1.inOut',delay:1});
-        tl.to(".gradient",{ opacity:1,ease:'power1.inOut'});
-        tl.to(".gradient",{width:'55%',duration:1,ease:'power1.inOut'});
-    },[]);
+         let tl = gsap.timeline();
+         tl.to(".gradient",{ x:0,opacity:0.8,duration:2,ease:'power1.inOut',delay:1.2});
+         tl.to(".gradient",{ opacity:1,ease:'power1.inOut'});
+         tl.to(".gradient",{width:'55%',duration:1,ease:'power1.inOut'});
+     },[]);
     return(
         <div className="conteinerGradient">
             <div className="gradient"></div>
