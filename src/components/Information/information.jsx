d919490996787dd2a2a03aeac425c28befcae2ea
juandrouville <jadrouville@gmail.com>
2025-03-18 20:10:47 -0300

    change responsive in fonts add clamp

--- src/components/Information/information.jsx
+++ src/components/Information/information.jsx
@@ -16,7 +16,7 @@ function Information() {
         gsap.to(".line",{width:'100%',duration:2,ease:"power1.inOut",opacity:1,delay:3});
         tl.to(".name",{ opacity:1,letterSpacing:"0.02em",duration:1,ease:"power1.inOut",delay:3});
         tl.to(".lastname",{opacity:1,letterSpacing:"0.04em",duration:2,ease:"power1.inOut"},"<");
-        tl.to(".imageJuan",{ delay:0.2,opacity:1,ease:"power1.inOut",clipPath:'inset(0 0 0 0)'},"<");
+        tl.to(".imageJuan",{ delay:0.5,opacity:1,ease:"power1.inOut",clipPath:'inset(0 0 0 0)'},"<");
         tl.to(".textPresentation",{ opacity:1,ease:"power1.inOut",clipPath:'inset(0 0 0 0)'},"<");
     })
     return (
