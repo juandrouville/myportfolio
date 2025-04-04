11c39d208b207f0f82f6ecdc91a198bdcfce1cbf
juandrouville <jadrouville@gmail.com>
2025-04-04 11:31:27 -0300

    animation button download

--- src/components/DownloadCv/ButtonDownload/ButtonDownload.jsx
+++ src/components/DownloadCv/ButtonDownload/ButtonDownload.jsx
@@ -1,14 +1,18 @@
-import React,{ useState }from "react";
+import React,{ useState, useEffect }from "react";
 import Modal from '../../Modal/Modal';
 import DownloadCv from "../DownloadCv";
 import './ButtonDownload.css';
 import { MdOutlineFileDownload } from "react-icons/md";
+import gsap from "gsap";
 
 function ButtonDownload(){
     const [openModal , setOpenModal]= useState(false);
+    useEffect(()=>{
+        gsap.to(".buttonAbsolute",{delay:2,duration:1.5, y:0,clipPath: 'inset(0 0 0 0)'});
+    })
     return (
         <div className="buttonAbsolute" onClick={() => {setOpenModal(true);console.log('onClick')}}>
-            <MdOutlineFileDownload color="#e1dfba" size={'0.8em'}/>
+            <MdOutlineFileDownload color="#e1dfba" size={'clamp( 1em ,1vw, 1.1em)'}/>
             <Modal open={openModal} onClickClose={()=>setOpenModal(false)}>
                 <DownloadCv/>
             </Modal>
