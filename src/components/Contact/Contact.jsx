052e697c7f6639f139f86d615729f972e62566e6
juandrouville <jadrouville@gmail.com>
2025-04-17 15:54:38 -0300

    Add responsive cardproyects and contact grid responsive

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React ,{ useContext } from "react";
+import React, { useContext } from "react";
 import './Contact.css';
 import { Language } from "../../context/languageContext";
 import linkedin from '../../icons/LINKEDIN.png';
@@ -10,11 +10,11 @@ import email from '../../icons/EMAIL.png';
 
 
 
-function Contact(){
-    const { language }= useContext(Language);
+function Contact() {
+    const { language } = useContext(Language);
     const title = {
-        esp:'CONTACTO',
-        por:'CONTATO',
+        esp: 'CONTACTO',
+        por: 'CONTATO',
     };
 
     return (
@@ -22,49 +22,49 @@ function Contact(){
             <h1>{title[language]}</h1>
             <div className="lineBottom"></div>
             <div className="conteinerIcons">
-                <div className="divisionIcons">
-                    <div className="links">
-                        <a href="https://www.linkedin.com/in/juan-drouville/" target={"_blank"} rel="noreferrer">
-                            <img src = {linkedin} alt='icon'/>
-                        </a>
-                        <h2>LINKEDIN</h2>
-                    </div>
-                    <div className="links">
-                        <a href="https://github.com/juandrouville" target={"_blank"} rel="noreferrer">
-                            <img src = {github} alt='icon'/>
-                        </a>
-                        <h2>GITHUB</h2>
-                    </div>
-                    <div className="links">
-                        <a href="https://gitlab.com/achudev" target={"_blank"} rel="noreferrer">
-                            <img src = {gitlab} alt='icon' />
-                        </a>
-                        <h2>GITLAB</h2>
-                    </div>
+                <div className="links">
+                    <a href="https://www.linkedin.com/in/juan-drouville/" target={"_blank"} rel="noreferrer">
+                        <img src={linkedin} alt='icon' />
+                    </a>
+                    <h2>LINKEDIN</h2>
                 </div>
-                <div className="divisionIcons">
-                    <div className="links">
-                        <a href="https://jadrouville@gmail.com" target={"_blank"} rel="noreferrer" >
-                            <img src={email} alt='icon'/>
-                        </a>
-                        <h2>EMAIL</h2>
-                    </div>
-                    <div className="links">
-                        <a href="https://api.whatsapp.com/send?phone=5548988488255&text=Hola%20Juan%20Drouville!" target={"_blank"} rel="noreferrer">
-                            <img src = {whatsapp} alt='icon'/>
-                        </a>
-                        <h2>WHATSAPP</h2>
-                    </div>
-                    <div className="links">
-                        <a href="https://www.instagram.com/juandrouville/" target={"_blank"} rel="noreferrer">
-                            <img src = {instagram} alt='icon'/>
-                        </a>
-                        <h2>INSTAGRAM</h2>
-                    </div>
+                <div className="links">
+                    <a href="https://github.com/juandrouville" target={"_blank"} rel="noreferrer">
+                        <img src={github} alt='icon' />
+                    </a>
+                    <h2>GITHUB</h2>
+                </div>
+                <div className="links">
+                    <a href="https://gitlab.com/achudev" target={"_blank"} rel="noreferrer">
+                        <img src={gitlab} alt='icon' />
+                    </a>
+                    <h2>GITLAB</h2>
+                </div>
+                <div className="links">
+                    <a href="https://jadrouville@gmail.com" target={"_blank"} rel="noreferrer" >
+                        <img src={email} alt='icon' />
+                    </a>
+                    <h2>EMAIL</h2>
+                </div>
+                <div className="links">
+                    <a href="https://api.whatsapp.com/send?phone=5548988488255&text=Hola%20Juan%20Drouville!" target={"_blank"} rel="noreferrer">
+                        <img src={whatsapp} alt='icon' />
+                    </a>
+                    <h2>WHATSAPP</h2>
+                </div>
+                <div className="links">
+                    <a href="https://www.instagram.com/juandrouville/" target={"_blank"} rel="noreferrer">
+                        <img src={instagram} alt='icon' />
+                    </a>
+                    <h2>INSTAGRAM</h2>
                 </div>
             </div>
         </div>
     );
 };
 
+
 export default Contact;
+
+
+
