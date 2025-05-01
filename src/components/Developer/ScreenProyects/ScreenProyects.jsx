2bd19508e8c3aad94033613b328a2bd92f615ae2
juandrouville <jadrouville@gmail.com>
2025-05-01 14:47:22 -0300

    Changes proyects screen

--- src/components/Developer/ScreenProyects/ScreenProyects.jsx
+++ src/components/Developer/ScreenProyects/ScreenProyects.jsx
@@ -2,22 +2,33 @@ import React, {useState} from "react";
 import './ScreenProyects.css';
 import CardProyect from "../CardProyect/CardProyect";
 import Modal from "../../Modal/Modal";
-import { objSaturno, objWine, objDog, objMyPortfolio } from './proyects';
+import { proyects } from './proyects';
 
 function ScreenProyects(){
-    const [openModal, setOpenModal] = useState(false);
+    console.log('objDog',proyects);
+    const [selectedProyect, setSelectedProyect] = useState(null);
+
+    const handleProyectClick = (proyect) => { setSelectedProyect(proyect) };
+    const handleCloseModal = () => { setSelectedProyect(null) };
+
     return (
         <div className="conteinerProyects">
-            <h1>PROJETOS</h1>
-            <div className="proyectsList" onClick={()=>setOpenModal(true)}>
-                <CardProyect  {...objSaturno}/>
-                {/* <CardProyect {...objWine}/>
-                <CardProyect {...objDog}/>
-                <CardProyect {...objMyPortfolio}/> */}
+            <h1 className="titleScreenProyects">PROJETOS</h1>
+            <div className="proyectsList">
+                {proyects.map(( obj, index )=>{
+                    return(
+                        <div key={index} className="cardProyect" onClick={()=>handleProyectClick(obj)}>
+                            <h1 className="titleCardProyect">{obj.title}</h1>
+                            <h1 className="subTitleCardProyect"> WEB APP </h1>
+                        </div> 
+                    )
+                })}
+                {selectedProyect && (
+                    <Modal open={!!selectedProyect} onClickClose={handleCloseModal}>
+                        <CardProyect {...selectedProyect} />
+                    </Modal>
+                )}
             </div>
-            <Modal open={openModal} onClickClose={() => setOpenModal(false)}>
-                <CardProyect  {...objDog}/>
-            </Modal>
         </div>
     );
 };
--- src/components/Developer/ScreenProyects/proyects.js
+++ src/components/Developer/ScreenProyects/proyects.js
@@ -1,14 +1,14 @@
  
-export const objDog = {
+const objDog = {
     description:{
-        esp:"Aplicación Web, proyecto personal para curso fullstack de Soy Henry, utilice:",
-        por:"Aplicativo Web, projeto pessoal para curso fullstack de Soy Henry, utilizei:",
+        esp:"Crie una web que consume datos de una API externa y los complementa con registros personalizados.\n  Usé React.js con Redux para gestión de estado, paginación dinámica y filtros avanzados. Css puro con diseño responsive. Backend en NodeJs y Express con PostgreSQL para almacenamiento. La solución ofrece CRUD completo, permitiendo desde consultas, creación e edición de nuevos registros",
+        por:"Desenvolvi um site que consome dados de uma API externa e complementa com registros personalizados.\n Utilizei React.js com Redux para gerenciamento de estado, paginação dinâmica e filtros avançados. CSS puro com design responsivo. Backend em NodeJs e Express com Postgre para armazenamento. A solução oferece CRUD completo, permitindo desde consultas até a criação e edição registros.",
     },
     title: 'MY DOG APP', 
     goSite:'https://mydog-app.web.app/',
     tools:["Html","JavaScript","NodeJS","Css","ReactJS","Redux","Postgress","Sequelize"],
 };
-export const objWine = {
+const objWine = {
     description: {
      esp:"Aplicación Web, proyecto grupal para el curso fullstack de Soy Henry, utilice:",
      por:"Aplicativo Web, projeto de grupo para curso fullstack de Soy Henry, utilizei",
@@ -17,7 +17,7 @@ export const objWine = {
     title: 'WINE APP',
     tools:["Html","JavaScript","NodeJS","Css","ReactJS","Redux","Postgress","Sequelize"],
 };
-export const objSaturno = {
+const objSaturno = {
     description : {
         esp:"Aplicación mobile para sistema de salud argentino,\n donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,",
         por:"Aplicativo mobile para o sitema de saúde argentino, \n onde atuei como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,donde actue como front-end,",
@@ -27,7 +27,7 @@ export const objSaturno = {
     tools:["JavaScript","NodeJS","React Native","Expo","Css"],
 };
 
-export const objMyPortfolio = {
+const objMyPortfolio = {
     description:{
         esp:"Mi propio sitio web, desarrollado y diseñado por mi,donde utilize todas las herramientas que tengo para mostrar mi trabajo y ademas, mostrar de lo que seria capaz de entregar.",
         por:"Meu proprio site web, desenvolvido e desenhado por mim, onde utilizei todas as ferramentas que conheço e apresentei o que eu posso oferecer." 
@@ -36,3 +36,5 @@ export const objMyPortfolio = {
     title:'MY PORTFOLIO',
     tools:["JavaScript","NodeJS","ReactJS","Css"],
 };
+
+export const proyects = [objDog,objWine,objSaturno,objMyPortfolio];
