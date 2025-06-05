ae6351f373d05082dfa1636a8bdebdf377787ec9
juandrouville <jadrouville@gmail.com>
2025-06-04 23:10:58 -0300

    animate screen proyects list

--- src/components/Developer/ScreenProyects/ScreenProyects.jsx
+++ src/components/Developer/ScreenProyects/ScreenProyects.jsx
@@ -1,11 +1,21 @@
-import React, {useState,useContext} from "react";
+import React, {useState,useContext, useEffect} from "react";
 import './ScreenProyects.css';
 import { Language } from "../../../context/languageContext";
 import CardProyect from "../CardProyect/CardProyect";
 import Modal from "../../Modal/Modal";
 import { proyects } from './proyects';
+import { gsap } from "gsap";
 
 function ScreenProyects(){
+
+    useEffect(()=>{
+        let tl = gsap.timeline();
+        tl.to(".titleScreenProyects",{opacity:1, y:0, duration:1,ease:"power1.inOut"});
+        tl.to(".lineTitle",{width:"90%",opacity:1,duration:0.8,ease:"power1.inOut"},"<");
+        tl.to(".cardProyect",{opacity:1,y:0,duration:0.4,ease:"power1.inOut",stagger:0.2},"<");
+        tl.to(".subTitleCardProyect",{width:"98%",duration:0.4,stagger:0.2});
+
+    },[]);
 
     const { language } = useContext(Language);
     const title = {
@@ -21,6 +31,7 @@ function ScreenProyects(){
     return (
         <div className="conteinerProyects">
             <h1 className="titleScreenProyects">{title[language]}</h1>
+            <div className="lineTitle"></div>
             <div className="proyectsList">
                 {proyects.map(( obj, index )=>{
                     return(
