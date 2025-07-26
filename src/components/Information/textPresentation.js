bac206a23838b0f8db9e140e22bbd7d3515c8e1e
juandrouville <jadrouville@gmail.com>
2025-07-26 19:59:37 -0300

    add new favicon edit text presentation

--- src/components/Information/textPresentation.js
+++ src/components/Information/textPresentation.js
@@ -1,4 +1,4 @@
 export const myPresentationText ={
-    esp: 'Soy Juan. Diseñador y programador enfocado en crear y resolver. Creo que el compromiso y la creatividad marcan la diferencia, por eso entrego mi máximo en cada proyecto.',
-    por: 'Oi, sou Juan. Designer e desenvolvedor focado em criar e resolver. Acredito que compromisso e criatividade fazem toda a diferença, por isso me dedico ao máximo em cada desafio.',
+    esp: 'Soy Juan Drouville.\n Diseñador y programador enfocado en crear y resolver.',
+    por: 'Oi, sou Juan Drouville. \n Designer e desenvolvedor focado em criar e resolver.',
 }
